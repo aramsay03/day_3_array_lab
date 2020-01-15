bea9ebbbf71b4011c5367fab9ec15c29be35bcd0
aramsay03 <alan@aramsay.co.uk>
2020-01-15 17:05:05 +0000

    Inital Commit

---
+++ start_point/my_functions.rb
@@ -0,0 +1,27 @@
+def add_array_lengths(array1, array2)
+  return array1.length() + array2.length()
+end
+
+
+def sum_array(array)
+  total = 0
+  for number in array
+    total += number
+  end
+  return total
+end
+
+
+def is_item_in_array(array, item)
+  for house in array
+    if item == house
+       return true
+    end
+  end
+  return false
+end
+
+
+def get_first_key( hash )
+  return hash.keys[0]
+end
